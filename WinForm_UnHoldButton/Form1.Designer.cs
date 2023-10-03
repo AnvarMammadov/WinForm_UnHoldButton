7add4a467c9024a43d391345f895bad575eeca08
name <anvarmammadovv@gmail.com>
2023-10-03 17:56:17 +0400

    Add project files.

---
+++ WinForm_UnHoldButton/Form1.Designer.cs
@@ -0,0 +1,67 @@
+﻿namespace WinForm_UnHoldButton
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnClickMe = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnClickMe
+            // 
+            this.btnClickMe.BackColor = System.Drawing.Color.Aqua;
+            this.btnClickMe.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnClickMe.Location = new System.Drawing.Point(380, 213);
+            this.btnClickMe.Name = "btnClickMe";
+            this.btnClickMe.Size = new System.Drawing.Size(137, 80);
+            this.btnClickMe.TabIndex = 0;
+            this.btnClickMe.Text = "Try Hold";
+            this.btnClickMe.UseVisualStyleBackColor = false;
+            this.btnClickMe.MouseHover += new System.EventHandler(this.btnClickMe_MouseHover);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(26F, 62F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Teal;
+            this.ClientSize = new System.Drawing.Size(932, 673);
+            this.Controls.Add(this.btnClickMe);
+            this.Font = new System.Drawing.Font("Segoe UI", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Margin = new System.Windows.Forms.Padding(10, 11, 10, 11);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnClickMe;
+    }
+}
+
---
+++ WinForm_UnHoldButton/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinForm_UnHoldButton
+{
+    public partial class Form1 : Form
+    {
+        Random random = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private bool isEqualPoint(Point p)
+        {
+            var currentLoc = new Point(btnClickMe.Location.X, btnClickMe.Location.Y);
+            if (currentLoc.X == p.X && currentLoc.Y == p.Y) return true;
+            return false;
+
+        }
+
+        private int getPointIndex(int currentIndex)
+        {
+            while (true)
+            {
+                var index = random.Next(0, 5);
+                if (index != currentIndex) return index;
+            }
+        }
+
+        private void btnClickMe_MouseHover(object sender, EventArgs e)
+        {
+            var chcPointFlag = random.Next(0, 5);
+            var midPoint = new Point(384, 293);
+            var topLeftPoint = new Point(115, 106);
+            var botLeftPoint = new Point(85, 487);
+            var topRightPoint = new Point(703, 82);
+            var botRightPoint = new Point(727, 516);
+            var points = new Point[] { midPoint, topLeftPoint, botLeftPoint, topRightPoint, botRightPoint };
+
+            if (chcPointFlag == 0)
+            {
+                if (!isEqualPoint(points[0])) btnClickMe.Location = new Point(points[0].X, points[0].Y);
+                else
+                {
+                    btnClickMe.Location = new Point(points[getPointIndex(0)].X, points[getPointIndex(0)].Y);
+                }
+            }
+            else if (chcPointFlag == 1)
+            {
+                if (!isEqualPoint(points[1])) btnClickMe.Location = new Point(points[1].X, points[1].Y);
+                else
+                {
+                    btnClickMe.Location = new Point(points[getPointIndex(1)].X, points[getPointIndex(1)].Y);
+                }
+            }
+            else if (chcPointFlag == 2)
+            {
+                if (!isEqualPoint(points[2])) btnClickMe.Location = new Point(points[2].X, points[2].Y);
+                else
+                {
+                    btnClickMe.Location = new Point(points[getPointIndex(2)].X, points[getPointIndex(2)].Y);
+                }
+            }
+            else if (chcPointFlag == 3)
+            {
+                if (!isEqualPoint(points[3])) btnClickMe.Location = new Point(points[3].X, points[3].Y);
+                else
+                {
+                    btnClickMe.Location = new Point(points[getPointIndex(3)].X, points[getPointIndex(3)].Y);
+                }
+            }
+            else if (chcPointFlag == 4)
+            {
+                if (!isEqualPoint(points[4])) btnClickMe.Location = new Point(points[4].X, points[4].Y);
+                else
+                {
+                    btnClickMe.Location = new Point(points[getPointIndex(4)].X, points[getPointIndex(4)].Y);
+                }
+            }
+            var randomColor = Color.FromArgb(random.Next(255), random.Next(255), random.Next(255));
+            this.BackColor = randomColor;
+        }
+    }
+
+}
